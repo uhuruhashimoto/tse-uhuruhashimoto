47377778e7deb9ce3da0d593054542b42b2dc028
uhuruhashimoto <uhuru.g.hashimoto.23@dartmouth.edu>
2021-07-26 18:25:07 -0400

    refactored crawler

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -4,125 +4,146 @@
 * 
 * Uhuru Hashimoto CS 50 21X
 */
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h> //for parsing command line args
-//#include "../libcs50/*"
-#define NUM_SLOTS 200
-
-/*************************** FUNCTION DECLARATIONS ************************************/
-
-static void itemcount(void *arg, const char *key, void *item);
-void pagefetcher();
-void pagescanner();
+#include <string.h>
+//all of library
+#include "../libcs50/bag.h"
+#include "../libcs50/set.h"
+#include "../libcs50/counters.h"
+#include "../libcs50/hashtable.h"
+#include "../libcs50/counters.h"
+#include "../libcs50/jhash.h"
+#include "../libcs50/memory.h"
+#include "../libcs50/webpage.h"
+#include "../libcs50/file.h"
 
 /*************************** DRIVER ************************************/
+//Note: allocated url is freed automatically upon crawler call to webpage_delete()
 int main(const int argc, char **argv) {
+	int status = 0;
 
 	/*------------------------------- CHECK ARGS ---------------------------*/
-	//check command-line args
-	//note: strings for arguments declared below
-	int max_depth; //depth to crawl
-
+	//CHECK NUMBER
 	if (argc < 4) {
 		fprintf(stderr, "Error: fewer than mininmum number of arguments.\n "
 			"Usage: ./crawler seedURL pageDirectory maxDepth\n");
-		return 1;
+		return ++status;
 	}
 	else if (argc > 4) {
 		fprintf(stderr, "Error: exceeded max number of arguments.\n "
 			"Usage: ./crawler seedURL pageDirectory maxDepth\n");
-		return 1;
+		return ++status;
 	}
-	//assign arguments
-	char *seedURL = argv[1];
 
-	//verify directory
+	//CHECK URL
+	//internal check
+	if (!isInternalURL(argv[1])) {
+		fprintf(stderr, "Error: Seed URL %s is not internal\n", argv[1]);
+		return ++status;
+	}
+	
+	//copy and normalize
+	char *url = copyURL(argv[1]);
+	if (!NormalizeURL(url)) {
+		fprintf(stderr, "Error: Seed URL %s cannot be normalized\n", url);
+		return ++status;
+	}
+
+	//CHECK DIR
 	char *dirname = argv[2];
-	//TODO use pagedir here
+	printf("Checking dir! TODO\n");
 
-	//assign maxDepth
-	max_depth = strtoi(argv[3], NULL, 10); //if non-numeric input, default to 0
+	//CHECK DEPTH
+	//if non-numeric input, default to 0
+	char *chardep = argv[3];
+	intdep = strtoi(argv[3], NULL, 10); 
 
 	/*------------------------------- DEPLOY CRAWLER ---------------------------*/
-	int status = crawler(seedURL, dirname, max_depth);
+	status += crawler(url_copy, dirname, chardep, intdep);
 
 	return status;
 }
 
+
 /*************************** HELPERS ************************************/
 //crawler
-int crawler(char *seedURL, char *dirname, int max_depth) {
-	/*------------------------------- INITIALIZE MODS ---------------------------*/
-	//crawler and its data structures
-	//all allocation here
-	int *nitem; //items in ht
+int crawler(char *seedURL, char *dirname, char *chardep, int intdep) {
+	//INITIALIZE DATA STRUCTURES
+	int ret = 0;
+	int *nitem = malloc(sizeof(int)); //counts items in ht
 	int doc_id = 0; // doc id for saved info
-	bag_t *bag = bag_new();
-	hashtable_t *table = table_new(NUM_SLOTS);
 
+	//bag (future pages)
+	bag_t bag = bag_new();
+	//hashtable (visited pages)
+	hashtable_t table = hashtable_new();
 	if (bag == NULL || table == NULL) {
-		fprinf(stderr, "Failed to initialize modules. Memory allocation error.\n");
-		return 2;
+		fprintf(stderr, "Failed to initalize crawler data structures.\n");
+		return (ret+=2);
 	}
 
-	nitem = malloc(sizeof(int));
-
-
-	/*------------------------------- CRAWL ---------------------------*/
-
-	//make the first webpage object (seed)
-	webpage_t *first = webpage_new(seedURL, max_depth, NULL); //no html yet
-	//put it in bag
-	if (bag_insert(bag, first) == NULL) {
-		fprintf(stderr, "Initialization/insertion error: bag not empty\n");
-		return 2;
+	webpage_t *firstpage = webpage_new(seedURL, intdep, NULL);
+	if (firstpage == NULL) {
+		fprintf(stderr, "Seed webpage initalization failed. Cannot proceed\n");
+		return (ret+=2);
 	}
 
-	//crawling loop
-	//get size of bag
-	hashtable_iterate(table, nitem, itemcount);
-	while (*nitem > 0) {
-		//get webpage
-		webpage_t *webpage = bag_extract(bag);
-		if (webpage == NULL) {
-			fprintf(stderr, "Bag extraction error while crawling\n");
-			return 3;
-		} //pause built in 
+	//insert into bag and ht
+	if (bag_insert(bag, firstpage) == NULL || hashtable_insert(bag, firstpage) == NULL) {
+		fprintf(stderr, "Seed webpage insertion failed. Cannot proceed\n");
+		return (ret+=2);
+	}
+	
 
-		//write page data to directory
-			//pagefetcher, pagescanner (docid++)
-
-		//if webpage is < depth, get urls and store them
-		if (webpage->depth < max_depth) {
-			//use pagescanner to get links from page
-			if (!pagefetcher(webpage)) {
-				fprintf(stderr, "Failed to find html data\n");
-				return 3;
+	//CRAWL
+	while (!isBagEmpty(bag, nitem)) {
+		//get webpage from bag
+		webpage_t *page = bag_extract(bag);
+		if (page != NULL) {
+			//fetch page from url
+			if (pagefetcher(page)) {
+				//save page
+				pagesaver(page);
+				//if we're still in depth constraint
+				if (webpage_getDepth(page) < intdep) {
+					//initalize tools for getting urls (allocation not necessary)
+					int pos = 0;
+					char *next_url;
+					//get urls
+					while ((next_url = webpage_getNextURL(page, &pos)) != NULL) {
+						//make into a webpage
+						webpage *newpage = webpage_new(next_url, ++webpage_getDepth(page), NULL);
+						//put in bag
+						bag_insert(bag, newpage);
+					}
+				}
 			}
-			else {
-
-			}
-			char *html = webpage_getHTML(page);
-
-			//for each page
-				//normalize urls
-				//check if seen: insert into ht (seen)
-				//if not seen yet, make webpage object (maxdepth++)
-				//insert into bag 
+			webpage_delete(page); //page no longer in bag; delete
 		}
-
-		hashtable_iterate(table, nitem, itemcount); //recheck if ht is empty
 	}
 
-
-	/*------------------------------- FREE MODS ---------------------------*/
-	//all deallocation here
+	//FREE STRUCTURES
 	free(nitem);
 	bag_delete(bag, webpage_delete());
 	hashtable_delete(hashtable, webpage_delete());
 
-	return 0;
+	return ret;
+}
+
+//copy to allocate first url
+//responsiblity of caller to free returned pointer
+char *copyURL(char *url) {
+	char *url_copy = assertp(calloc(sizeof(char), strlen(url)+1), "Memory insufficient");
+	strcpy(url_copy, url);
+	return url_copy;
+}
+
+//assumes non-null webpage with fetched html content
+void pagesaver(webpage_t *webpage) {
+	//saves and writes to file
+	fprintf(stdout, "saving page! TODO\n");
 }
 
 //assumes null webpage html
@@ -131,16 +152,18 @@ bool pagefetcher(webpage_t *webpage) {
 	return webpage_fetch(webpage);
 }
 
-void pagescanner() {
-	 webpage_fetch()
+//wrapper to check if bag is empty
+bool isBagEmpty(bag_t bag, int *counter) {
+	bag_iterate(bag, counter, itemcount);
+	if (*counter > 0) {
+		return false;
+	}
+	return true;
 }
 
-void pagesaver() {
-
-}
-
-
+//helper
 //gets size of bag
+//called as pointer to check if bag empty
 static void itemcount(void *arg, const char *key, void *item)
 {
   int *nitems = arg;
