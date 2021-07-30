8db2c4ab03e742c4ca1539b0e89355aefefa82d1
Uhuru G. Hashimoto <f003mfq@plank.thayer.dartmouth.edu>
2021-07-30 10:15:34 -0400

    RESUBMISSION: modified testing protocol that includes directory creation

--- crawler/testing.sh
+++ crawler/testing.sh
@@ -2,16 +2,16 @@
 
 seedA=http://cs50tse.cs.dartmouth.edu/tse/letters/index.html
 externalseed=http://google.com/
-dirA=test
-dirB=test1
-dirC=test2
-dirD=test3
+dir=test
+dirA=$dir/letters
 depthA=0
-depthB=1
-depthC=2
-depthD=3
 negdepth=-3
 largedepth=100
+
+####### CLEAN / CREATE DIRECTORIES #########
+rm -rf $dirA-{0..3}
+
+mkdir -p $dirA-{0..3}
 
 # incorrect num of input arguments
 # too few
@@ -22,27 +22,27 @@ largedepth=100
 ./crawler $seedA $dirA $depthA $depthA
 
 # negative depth provided
-./crawler $seedA $dirA $negdepth
+./crawler $seedA $dirA-0 $negdepth
 
 # depth exceeds max (50)
-./crawler $seedA $dirA $largedepth
+./crawler $seedA $dirA-0 $largedepth
 
 # non-internal URL
 ./crawler $externalseed $dirA $depthA
 
 ################### TEST SITE ####################
 # please note: if not in safe mode (file overwrite check enabled), 
-# data will be overwritten and test directory will contain data from depth 3.
+# data may be overwritten.
 
 # DEPTH 0
-./crawler $seedA $dirA $depthA
+./crawler $seedA $dirA-0 0
 # DEPTH 0 - SAME DIRECTORY (overwrite check)
-./crawler $seedA $dirA $depthA
+./crawler $seedA $dirA-0 0
 # DEPTH 1
-./crawler $seedA $dirB $depthB
+./crawler $seedA $dirA-1 1
 # DEPTH 2
-./crawler $seedA $dirC $depthC
+./crawler $seedA $dirA-2 2
 # DEPTH 3
-./crawler $seedA $dirD $depthD
+./crawler $seedA $dirA-3 3
 
 
