52678126652f9c04faa212ae92388c77e6463eeb
uhuruhashimoto <uhuru.g.hashimoto.23@dartmouth.edu>
2021-08-03 19:58:42 -0400

    started index files

--- indexer/indexer.c
+++ indexer/indexer.c
@@ -4,20 +4,32 @@
 * Uhuru Hashimoto CS50 21X
 */
 
-#include <stdio.h>
 #include <stdlib.h>
+#include "../libcs50/bag.h"
+#include "../libcs50/set.h"
+#include "../libcs50/counters.h"
+#include "../libcs50/hashtable.h"
+#include "../libcs50/counters.h"
+#include "../libcs50/jhash.h"
+#include "../libcs50/memory.h"
+#include "../libcs50/webpage.h"
+#include "../libcs50/file.h"
+#include "../common/pagedir.h"
 
 //driver
-main () {
+int main (const int argv, char *argc[]) {
+	int status = 0;
 
+	return status;
 }
 
 //NEW: builds index from crawler output
-index_build() {
-
+index_t *index_build(char *dirname) {
+	
 }
 
 //helper
-index_page() {
+//handles one file of name (dirname/doc_id)
+static void index_page(char *filename) {
 
 }
