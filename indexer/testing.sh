0af373f6c1f79e238241d917ebbca7c2899affaa
Uhuru G. Hashimoto <f003mfq@plank.thayer.dartmouth.edu>
2021-08-05 19:45:21 -0400

    more testing depths and corresponding files ignored

--- indexer/testing.sh
+++ indexer/testing.sh
@@ -1,6 +1,6 @@
 # Testing file for indexer and indextest
-rm -f testfile0 testfile1 newfile
-touch testfile0 testfile1 newfile
+rm -f testfile0 testfile1 testfile2 newfile0 newfile1 newfile2
+touch testfile0 testfile1 testfile2 newfile0 newfile1 newfile2
 
 # Indexer
 
@@ -23,6 +23,9 @@ echo "Testing letters depth 0..."
 echo "Testing letters depth 1..."
 ./indexer ../crawler/test/letters/letters-1 testfile1
 
+echo "Testing letters depth 2..."
+./indexer ../crawler/test/letters/letters-2 testfile2
+
 # Indextest
 
 # No args
@@ -35,5 +38,11 @@ echo "Testing letters depth 1..."
 ./indextest onearg twoarg threearg
 
 echo "Testing indextest with depth 0..."
-./indextest testfile0 newfile
+./indextest testfile0 newfile0
+
+echo "Testing indextest with depth 1..."
+./indextest testfile1 newfile1
+
+echo "Testing indextest with depth 2..."
+./indextest testfile2 newfile2
 
