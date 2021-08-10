6f12d2d50e8f1b3fa9ef9f7b192fcb222ad40aea
Uhuru G. Hashimoto <f003mfq@plank.thayer.dartmouth.edu>
2021-08-10 10:39:00 -0400

    full error checking or queries; need searching logic

--- querier/querier.c
+++ querier/querier.c
@@ -7,6 +7,8 @@
 //standard macros
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 //library macros
 #include "../libcs50/bag.h"
 #include "../libcs50/set.h"
@@ -23,8 +25,13 @@
 
 /**************** FUNCTION DECLARATIONS ***********************/
 int query_user(index_t *index, char *dirname);
+bool hasSyntaxErrors(char *query);
+bool beginsWith(char *line, char *prefix);
+bool endsWith(char *line, char *suffix);
+bool isAlphabet(char *line);
+void search_query(char *line);
 
-/*************** STATIC/LOCAL FUNCTIONS ***********************/
+/*************** STATIC/LOCAL FUNCTIONS (HELPERS) ***********************/
 
 
 // driver; checks arguments and initiates loop
@@ -65,9 +72,95 @@ int main(const int argc, char **argv) {
 
 int query_user(index_t *index, char *dirname) {
     int ret = 0;
+    char *line = NULL;
 
-    //query user
-    //use readlinep
-
+    // query user
+    while ((line = freadlinep(stdin)) != NULL) { // read lines until EOF reached
+        if (!hasSyntaxErrors(line) && strlen(line) > 0) {
+            fprintf(stdout, "%s\n", line);
+            search_query(line);
+            free(line);
+        }
+        else { // normalization error (handled below) or empty query
+            free(line);
+            return ++ret;
+        }
+    }
     return ret;
+}
+
+// query helper: checks for syntax errors and prints a specific error for each case
+bool hasSyntaxErrors(char *query) {
+    // Error cases: 
+    // normalization fails
+    if (!NormalizeWord(query)) {
+        fprintf(stderr, "Error: failed to normalize query\n");
+        return true;
+    }
+    // non-alphabetic character encountered
+    if (!isAlphabet(query)) {
+        fprintf(stderr, "Error: query must consist solely of alphabetic characters\n");
+        return true;
+    }
+    // query begins with "and" or "or" (already normalized)
+    if (beginsWith(query, "and") || beginsWith(query, " and")) {
+        fprintf(stderr, "Error: query may not begin with an \"and\" operator\n");
+        return true;
+    }
+    if (beginsWith(query, "or") || beginsWith(query, " or")) {
+        fprintf(stderr, "Error: query may not begin with an \"or\" operator\n");
+        return true;
+    }
+    
+    // query ends with "and" or "or"
+    if (endsWith(query, "and") || endsWith(query, "and ")) {
+        fprintf(stderr, "Error: query may not end with an \"and\" operator\n");
+        return true;
+    }
+    if (endsWith(query, "or") || endsWith(query, "or ")) {
+        fprintf(stderr, "Error: query may not end with an \"or\" operator\n");
+        return true;
+    }
+
+    return false;
+}
+
+// helper: checks character by character to see if query begins with prefix
+bool beginsWith(char *line, char *prefix) {
+    if (strlen(prefix) > strlen(line)) return false; //length check 
+    for (int i = 0; i < strlen(prefix); i++) {
+        if (line[i] != prefix[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// helper: checks character by character to see if query ends with prefix
+bool endsWith(char *line, char *suffix) {
+    if (strlen(suffix) > strlen(line)) return false; //length check
+    for (int i = 0; i < strlen(suffix); i++) {
+        int line_index = strlen(line) - i;
+        int suff_index = strlen(suffix) - i;
+        if (line[line_index] != suffix[suff_index]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// helper: checks that query is made of alphabetic characters and spaces
+bool isAlphabet(char *line) {
+    for (int i = 0; i < strlen(line); i++) {
+        if (!isalpha(line[i])) {
+            if (line[i] != ' ') {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+void search_query(char *line) {
+    fprintf(stdout, "Searching query...\n");
 }
--- querier/testing.sh
+++ querier/testing.sh
@@ -10,3 +10,7 @@
 
 # incorrect file
 ./querier ../crawler/test/letters/letters-0 wrongfile
+
+# Test - letters depth 0
+echo "Testing with letters depth 0..."
+./querier ../crawler/test/letters/letters-0 ../indexer/test/letters/index.letters-0
